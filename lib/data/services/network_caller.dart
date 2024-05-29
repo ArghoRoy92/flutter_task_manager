ceb8834f0987c4be0032d34ecad814db3987b36b
Ar G Ho <argho.roy488@gmail.com>
2024-05-30 01:39:00 +0600

    NetworkCaller

---
+++ lib/data/services/network_caller.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'package:flutter_task_manager/data/model/network_response.dart';
+import 'package:http/http.dart';
+
+class NetworkCaller {
+  Future<NetworkResponse> getRequest(String url) async {
+    try {
+      Response response = await get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        return NetworkResponse(
+            true, response.statusCode, jsonDecode(response.body));
+      } else {
+        return NetworkResponse(false, response.statusCode, null);
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+    return NetworkResponse(false, -1, null);
+  }
+
+  Future<NetworkResponse> postRequest(
+      String url, Map<String, dynamic> body) async {
+    try {
+      Response response = await post(Uri.parse(url));
+      if (response.statusCode == 200) {
+        return NetworkResponse(
+            true, response.statusCode, jsonDecode(response.body));
+      } else {
+        return NetworkResponse(false, response.statusCode, null);
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+    return NetworkResponse(false, -1, null);
+  }
+}
