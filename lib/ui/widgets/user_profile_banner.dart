053d5d676cfeca253ff7594ea08f10f2074367b9
Ar G Ho <argho.roy488@gmail.com>
2024-06-15 21:30:49 +0600

    Logout Button Has Been Added

--- lib/ui/widgets/user_profile_banner.dart
+++ lib/ui/widgets/user_profile_banner.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_task_manager/data/model/auth_utility.dart';
+import 'package:flutter_task_manager/ui/screens/authentication_screens/login_screen.dart';
 
 class UserProfileBanner extends StatelessWidget {
   const UserProfileBanner({
@@ -13,31 +14,45 @@ class UserProfileBanner extends StatelessWidget {
       color: Colors.green,
       child: Row(
         children: [
-          CircleAvatar(
+          const CircleAvatar(
             backgroundColor: Colors.grey,
             radius: 25,
           ),
-          SizedBox(
+          const SizedBox(
               width: 10), // Adds space between the CircleAvatar and the text
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                (AuthUtility.userInfo.data?.firstName ?? ' ') +
-                    ' ' +
-                    (AuthUtility.userInfo.data?.lastName ?? ' '),
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w500,
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '${AuthUtility.userInfo.data?.firstName ?? ' '} ${AuthUtility.userInfo.data?.lastName ?? ' '}',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
-              ),
-              Text(
-                AuthUtility.userInfo.data?.email ?? ' ',
-                style: TextStyle(
-                  color: Colors.white,
+                Text(
+                  AuthUtility.userInfo.data?.email ?? ' ',
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
+          ),
+          IconButton(
+            onPressed: () async {
+              await AuthUtility.clearUserInfo();
+              Navigator.pushAndRemoveUntil(context,
+                  MaterialPageRoute(builder: (context) => const SignInScreen()),
+                  (route) {
+                return false;
+              });
+            },
+            icon: const Icon(
+              Icons.logout_outlined,
+              color: Colors.white,
+            ),
           ),
         ],
       ),
